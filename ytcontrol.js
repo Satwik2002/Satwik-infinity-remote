d3ed257997309bf85330f6f15c73cf6dc2861398
Satwik2002 <satwik2002.ss@gmail.com>
2021-06-27 16:38:19 +0530

    Basic function

---
+++ ytcontrol.js
@@ -0,0 +1,18 @@
+const column_number=3;
+var left_arrow = new KeyboardEvent("keyup" ,{keyCode: 37});
+var up_arrow = new KeyboardEvent("keyup" ,{keyCode: 38});
+var right_arrow = new KeyboardEvent("keyup" ,{keyCode: 39});
+var down_arrow = new KeyboardEvent("keyup" ,{keyCode: 40});
+
+let index=0;
+let keys = {37: -1,38: -column_number,39: +1,40: +column_number};
+const vdbox = document.querySelectorAll("#content.style-scope ytd-rich-item-renderer");
+document.addEventListener("keyup", event => {
+       vdbox[index].style.backgroundColor = "white";
+	   index+=keys[event.keyCode];
+	   if(index<0)index=0;
+       vdbox[x].style.backgroundColor = "red";
+})
+
+
+   
